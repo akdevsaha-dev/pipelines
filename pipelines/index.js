9596b76d797af4dc7dd6c3dec85c93543f24ed28
Akdev Saha <akdevsaha2003@gmail.com>
2026-03-01 21:17:04 +0530

    delete pipeline folder

--- pipelines/index.js
+++
@@ -1,3 +0,0 @@
-console.log("hello")
-console.log("Learning ci/cd with Github Action")
-
--- pipelines/test.js
+++
@@ -1,3 +0,0 @@
-console.log("starting the test");
-setTimeout(() => console.log("Waiting 3 seconds ..."), 3000);
-console.log("Test complete");
